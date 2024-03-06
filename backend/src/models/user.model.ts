c970052ba2d3c825b77c0ffe1ec876f1c13d5ba9
Liad Yacobi <liadyacobi@gmail.com>
2024-03-06 20:34:44 +0200

    create initial user mongoose schema

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+export interface IUser {
+  email: string
+  password: string
+  imgUrl?: string
+  _id?: string
+  refreshTokens?: string[]
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  email: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: false,
+  },
+  imgUrl: {
+    type: String,
+    required: false,
+  },
+  refreshTokens: {
+    type: [String],
+    required: false,
+  },
+})
+
+export default mongoose.model<IUser>('User', userSchema)
