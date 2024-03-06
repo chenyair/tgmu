fa08fab19848461ad2887f5c596341757c17a7a2
Liad Yacobi <liadyacobi@gmail.com>
2024-03-07 00:41:49 +0200

    Create a general logger

---
+++ backend/src/utils/logger.ts
@@ -0,0 +1,20 @@
+import winston from 'winston';
+
+const logFormat = winston.format.printf(
+  ({ level, message, label = 'general', timestamp }) =>
+    `${timestamp} [${label.toUpperCase()}] [${level.toUpperCase()}]: ${message}`
+);
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'debug',
+  format: logFormat,
+  defaultMeta: { service: 'backend' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'backend.log' }),
+    new winston.transports.Console(),
+  ],
+});
+
+// Export a function that receives a module name and returns a child logger for that module
+export default (module: string) => logger.child({ label: module });
