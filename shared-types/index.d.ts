ddc2407a98c5529fd8fbbd23bf5bf425ebab8ab4
Yair Chen <yair.chen2000@gmail.com>
2024-03-08 16:27:22 +0200

    feat(types): export user types

--- shared-types/index.d.ts
+++ shared-types/index.d.ts
@@ -3,6 +3,19 @@ declare namespace sharedTypes {
     accessToken: string;
     refreshToken: string;
   }
+
+  export interface IUser {
+    _id?: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    age: number;
+    imgUrl?: string;
+    refreshTokens?: string[];
+  }
+  
+  export type IUserDetails = Pick<IUser, '_id' | 'age' | 'email' | 'firstName' | 'lastName'>;
 }
 
 export = sharedTypes;
