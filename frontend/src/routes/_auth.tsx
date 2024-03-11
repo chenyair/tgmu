c5c40af269817e8351731f27b1193b1330753889
Yair Chen <yair.chen2000@gmail.com>
2024-03-11 11:47:36 +0200

    fix(logo): add logo to auth and fix colors css

--- frontend/src/routes/_auth.tsx
+++ frontend/src/routes/_auth.tsx
@@ -1,12 +1,16 @@
 import { Outlet, createFileRoute } from '@tanstack/react-router';
 import authBackground from '@/assets/new-auth-background.jpg';
+import tgmuLogo from '@/assets/tgmu-logo.svg';
 import './_auth.scss';
 
 const AuthLayout: React.FC = () => {
   return (
     <div className="row h-100">
-      <div className="col-3 login-section p-2 h-100 d-flex justify-content-center align-items-center">
-        <Outlet />
+      <div className="col-3 login-section p-2 h-100 d-flex flex-column justify-content-center">
+        <div className="d-flex justify-content-center align-items-center flex-column gap-4">
+          <img src={tgmuLogo} alt="logo-icon" style={{ width: '10em' }} />
+          <Outlet />
+        </div>
       </div>
       <div className="col-9 p-0 h-100 d-flex justify-content-center align-items-center auth-image">
         <img src={authBackground} style={{ width: '100%' }} />
