fd4972069dacd9f7fdffe4b08e72876339b6a2fb
Yair Chen <yair.chen2000@gmail.com>
2024-03-13 01:32:53 +0200

    feat(google): add icon

--- frontend/src/pages/auth/components/google-sign-in-button.tsx
+++ frontend/src/pages/auth/components/google-sign-in-button.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import googleIcon from '@/assets/google.svg';
-import './google-sign-in-button.scss';
-
+import React, { useEffect } from 'react';
+import { useGoogleOAuth } from '@react-oauth/google';
+declare global {
+  interface Window {
+    google: any;
+  }
+}
 const GoogleSignInButton: React.FC = () => {
+  const { clientId, scriptLoadedSuccessfully } = useGoogleOAuth();
+  useEffect(() => {
+    if (!scriptLoadedSuccessfully) return;
+
+    window.google.accounts.id.initialize({
+      client_id: clientId,
+      callback: handleCredentialResponse,
+    });
+    window.google.accounts.id.renderButton(document.getElementById('google-signin-btn'), {
+      shape: 'circle',
+    });
+  }, [scriptLoadedSuccessfully, clientId]);
+
+  const handleCredentialResponse = (response: any) => {
+    console.log('ID: ' + response.credential);
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center btn-google-sign-in p-2">
-      <img alt="google-icon" className="google-icon me-2" src={googleIcon} />
-      <span className="roboto-medium">Sign in with Google</span>
+      <button id="google-signin-btn"></button>
     </div>
   );
 };
