d7fc9ccc1bfcc84668a6b4a5f44ba764e478fe22
liadyacobi <65843947+liadyacobi@users.noreply.github.com>
2024-03-14 10:01:30 +0200

    Update frontend/src/components/divider.tsx

--- frontend/src/components/divider.tsx
+++ frontend/src/components/divider.tsx
@@ -1,7 +1,7 @@
 interface DividerProps {
   className?: string;
 }
-const Divider: React.FC<DividerProps> = ({ classNames = 'py-3' }: DividerProps) => {
+const Divider: React.FC<DividerProps> = ({ className = 'py-3' }: DividerProps) => {
   return (
     <div className={`d-flex align-items-center ${classNames}`}>
       <div style={{ borderBottom: '0.1rem solid rgb(60,60,60)', width: '100%' }} />
